b68c834365a3fd36ce0ebd2b4cd9fd5948d4989c
Baka632 <stevemc123456@outlook.com>
2022-06-03 16:11:16 +0800

    将AssetStudio的文件内联至ArknightsResources.Utility中

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ArknightsResources.Controls.Uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Baka632  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
