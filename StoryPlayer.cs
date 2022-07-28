2e2041151e1c731e85bc5033e59f4b384950d4f3
Baka632 <stevemc123456@outlook.com>
2022-07-28 19:01:02 +0800

    Revert "将StoryPlayer从TemplateControl改为UserControl"
    This reverts commit 151c2b6699e797d0e6c200a729d1339675040af6.

--- StoryPlayer.cs
+++ StoryPlayer.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
 namespace ArknightsResources.Controls.Uwp
 {
     /// <summary>
     /// 表示可播放明日方舟剧情文件的控件
     /// </summary>
-    public sealed partial class StoryPlayer : UserControl
+    public sealed class StoryPlayer : Control
     {
         /// <summary>
         /// 初始化<seealso cref="StoryPlayer"/>类的新实例
         /// </summary>
         public StoryPlayer()
         {
-            this.InitializeComponent();
+            this.DefaultStyleKey = typeof(StoryPlayer);
         }
     }
 }
